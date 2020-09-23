a5cdc264366039a6b28b7bb8035964a5318dbe44
LuciaIng <71437871+LuciaIng@users.noreply.github.com>
2020-09-23 09:20:11 +0200

    Add files via upload

---
+++ media.py
@@ -0,0 +1,8 @@
+def media():
+    x1=input("Introduce el primer numero")
+    x2=input("Introduce el primer numero")
+    x3=input("Introduce el primer numero")
+    x_media=(x1+x2+x3)/3.0
+    print "la media vale ",x_media
+
+media()
