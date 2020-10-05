554640e46fb2e25a656535fff3f2aaf24bb60dfe
LuciaIng <71437871+LuciaIng@users.noreply.github.com>
2020-10-05 13:47:42 +0200

    Add files via upload

---
+++ ululador.py
@@ -0,0 +1,12 @@
+'''Haz un programa que lea una cadena y sustituya
+las vocales por la letra u. palabra de 10,For '''
+def ululador():
+    palabra=raw_input("Introduce una palabra para ulular: ")
+    for cont in range(0,10):
+        if (palabra[cont]=='a' OR palabra[cont]=='e'):
+            print 'u'
+        else:
+            print palabra[cont]
+    
+    
+ululador()
