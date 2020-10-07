47bfc21328a96bdf4f3021595fbef8a16b72ac3e
LuciaIng <71437871+LuciaIng@users.noreply.github.com>
2020-10-07 09:40:58 +0200

    Add files via upload

--- ululador.py
+++ ululador.py
@@ -3,7 +3,7 @@ las vocales por la letra u. palabra de 10,For '''
 def ululador():
     palabra=raw_input("Introduce una palabra para ulular: ")
     for cont in range(0,10):
-        if (palabra[cont]=='a' OR palabra[cont]=='e'):
+        if (palabra[cont]=='a' or palabra[cont]=='e'or palabra[cont]=='i'or palabra[cont]=='o'):
             print 'u'
         else:
             print palabra[cont]
