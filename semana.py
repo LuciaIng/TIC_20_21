8629acef30f00c4035bc9a7d8dbd63ca6ccf2905
LuciaIng <71437871+LuciaIng@users.noreply.github.com>
2020-10-28 09:20:16 +0100

    Add files via upload

---
+++ semana.py
@@ -0,0 +1,7 @@
+'''3'''
+def semana():
+    numero=input("Introduce un numero:")
+    semana=("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo")
+    print semana[numero-1]
+
+semana ()
