b4fbcad7ac4173050bdacfb5740abfa99365efcc
LuciaIng <71437871+LuciaIng@users.noreply.github.com>
2020-11-14 20:32:47 +0100

    Add files via upload

---
+++ ejercico_18.py
@@ -0,0 +1,9 @@
+def ejercicio_18():
+    nombre=raw_input("Introduce un nombre: ")
+    apellido1=raw_input("Introduce un apellido1: ")
+    apellido2=raw_input("Introduce un apellido2: ")
+
+    print "completo: ", nombre,apellido1,apellido2
+
+ejercicio_18()
+    
