e2b4be016c669bffe0dff88bc41b6e7c0185b4f5
LuciaIng <71437871+LuciaIng@users.noreply.github.com>
2021-01-05 17:39:35 +0100

    Update triangulo.cpp

--- triangulo.cpp
+++ triangulo.cpp
@@ -1,45 +1,28 @@
-//Función que dibuje un árbol de navidad
-#include<stdio.h>
-/*
->Introduzca la altura del árbol: 5
-		*
-	   ***
-	  *****
-	 ********
-    ***********
-    	*
-    	*
-    FELIZ NAVIDAD
-1.Empezamos por un problema más sencillo 
->Introduzca la altura del triangulo:5
-*
-**
-***
-****
-*****
-2. Lo complicamos un poco:
-	*
-   **
-  ***
- ****
-*****
-3. Intentamos la copa del árbol:
-para un árbol de 5
-for(fila=altura-1; fila>=0;fila--)
-fila=4 4 espacios + 1 asterisco
-fila=3 3 espacios + 3 asteriscos
-fila=2 2 espacios + 5 asteriscos
-fila=1 1 espacios + 7 asteriscos
-fila=0 0 espacios + 9 asteriscos
-*=m*fila+n
-*/
-int main(){
+//FunciÃ³n que dibuje un Ã¡rbol de navidad
+# include <stdio.h>
 
-	
-	for (fila=1;fila<=altura;fila++){
-		for(col=1;col<=fila,col++){
-			printf("*");
-		}
-		printf("\n");
-		}
+int main (){
+ int fila,aux,espacio,aux2;
+ const int NUM_FILAS = 5; 
+ const int TRONCO = 3;
+ const int POS_TRONCO = 6;
+ 
+ for (fila=0,aux=NUM_FILAS;fila<NUM_FILAS;fila++,aux--){
+  for(espacio=0;espacio<aux;espacio++)
+   printf(" ");
+   for(aux2=NUM_FILAS;aux2>=aux;aux2--){
+    if(aux2!=NUM_FILAS)
+     printf("*");
+    printf ("*");
+   }
+   printf ("\n");
+ }
+ 
+ for(aux=1;aux<=TRONCO;aux++){
+  for(fila=1;fila<POS_TRONCO;fila++)
+   printf(" ");
+   printf("*");
+   printf ("\n");
+ }
+ printf("FELIZ NAVIDAD");
 }
