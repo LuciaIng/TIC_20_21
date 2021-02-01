228823124f8c2bef66f2f239d3503a32a1b478cd
LuciaIng <71437871+LuciaIng@users.noreply.github.com>
2021-02-01 13:49:42 +0100

    Add files via upload

---
+++ matriz2.cpp
@@ -0,0 +1,41 @@
+/*
+Lee dos matrices 3x3 y devuelve la suma
+*/
+
+#include<stdio.h>
+
+int main(){
+	int matriz_1[3][3];
+	int matriz_2[3][3];
+	int fil,col;
+	int matriz_suma[3][3];
+	// Leo la matriz 1
+	for(fil=0;fil<3;fil++){
+		for(col=0;col<3;col++){
+			printf("\nfila %d, columna %d:", fil+1, col+1);
+			scanf("%d", &matriz_1[fil][col]);
+		}
+	}
+	//SUMA MATRICES
+	for(fil=0;fil<3;fil++){
+		for(col=0;col<3;col++){
+			matriz_suma[fil][col]=matriz_1[fil][col]+matriz_2[fil][col];
+		}
+	}
+	
+	//Muestro la matriz
+	printf("\n Has INTRODUCIDO LA MATRIZ : \n");
+	for(fil=0;fil<3;fil++){
+		for(col=0;col<3;col++){
+			printf("%d", matriz_1[fil][col]);
+		}
+		printf("\n");
+	}
+	
+	
+	// Leo la matriz 2
+	// Las sumo
+	// Muestro la matriz suma
+	
+	return 0;
+}
