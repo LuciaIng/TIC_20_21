cd0aef9011d2302c21375f6ff364db4de09d27fd
LuciaIng <71437871+LuciaIng@users.noreply.github.com>
2021-02-01 13:47:56 +0100

    Add files via upload

---
+++ puntero_4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//¿y los arrays?
+
+int main(){
+	char letras[10];
+	int cont;
+	letras[0]='H';
+	letras[1]='O';
+	letras[2]='L';
+	letras[3]='A';
+	for(cont=0;cont<10;cont++){
+		printf("%c", *(letras+cont));
+		//printf("%c",letras[cont]);
+	}
+	return 0;
+	
+}
