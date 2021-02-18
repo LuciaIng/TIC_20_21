60c17f084575a5ab8b07e474dc003dca7c9e608b
LuciaIng <71437871+LuciaIng@users.noreply.github.com>
2021-02-18 12:16:41 +0100

    Add files via upload

---
+++ puntero_6.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+	char*lista[3];
+	int cont;
+	char aux[20];
+	int longitud;
+	for(cont=0;cont<3;cont++){
+		printf("\npalabra %d: ",cont);
+		scanf("%s",aux);
+		longitud=strlen(aux);
+		lista[cont]=(char *) malloc(longitud);
+		strcpy(lista[cont],aux);//(destino,origen)
+		
+		
+	}		
+	//MOSTRAR LISTA
+	printf("\n**********************************");
+	printf("\n*   FABULOSA LISTA DE PALABRAS ");
+	printf("\n**********************************");
+	for(cont=0;cont<3;cont++){
+	printf("\n %d .- %s",*(lista+cont));
+	}
+	return 0;
+	
+}
